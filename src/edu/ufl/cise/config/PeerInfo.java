617be16cd839ae899783dc9d9615755ed3d768b0
Sanchit <saanchithefriend@gmail.com>
2015-03-06 05:10:25 -0500

    Added peerConfig reader.

--- src/edu/ufl/cise/config/PeerInfo.java
+++ src/edu/ufl/cise/config/PeerInfo.java
@@ -2,6 +2,48 @@ package edu.ufl.cise.config;
 
 public class PeerInfo {
 
+	String peerId;
+	String hostname;
+	int port;
+	boolean isCompleteFile;
 	
+	public PeerInfo(){}
+	
+	public PeerInfo( String peerId,
+			String hostname,
+			int port,
+			boolean isCompleteFile){
+		this.peerId = peerId;
+		this.hostname = hostname;
+		this.port = port;
+		this.isCompleteFile = isCompleteFile;
+	}
+
+	public String getPeerId() {
+		return peerId;
+	}
+
+	public void setPeerId(String peerId) {
+		this.peerId = peerId;
+	}
+	
+	public String getHostname() {
+		return hostname;
+	}
+	public void setHostname(String hostname) {
+		this.hostname = hostname;
+	}
+	public int getPort() {
+		return port;
+	}
+	public void setPort(int port) {
+		this.port = port;
+	}
+	public boolean isCompleteFile() {
+		return isCompleteFile;
+	}
+	public void setCompleteFile(boolean isCompleteFile) {
+		this.isCompleteFile = isCompleteFile;
+	}
 	
 }
--- src/edu/ufl/cise/util/PeerConfigReader.java
+++ src/edu/ufl/cise/util/PeerConfigReader.java
@@ -1,6 +1,41 @@
 package edu.ufl.cise.util;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+
+import edu.ufl.cise.config.PeerInfo;
+
 public class PeerConfigReader {
 	
 
+	public static HashMap<String, PeerInfo> configReader( File fileName, String filePath){
+		BufferedReader br = null;
+		HashMap<String, PeerInfo> peerMap = new HashMap<String, PeerInfo>();
+		try {
+			String line;
+			br = new BufferedReader(new FileReader(filePath + fileName));
+			while ((line = br.readLine()) != null) {
+				PeerInfo peerInfo = new PeerInfo();
+				String arr[] = line.split(" ");
+				String peerID = arr[0];
+				String hostName = arr[1];
+				int port = Integer.parseInt(arr[2]);
+				boolean isCompleteFile = Boolean.parseBoolean(arr[3]);
+				peerInfo = new PeerInfo(peerID, hostName, port, isCompleteFile);
+				peerMap.put(peerID, peerInfo);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (br != null)br.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+		return peerMap;
+	}
 }
