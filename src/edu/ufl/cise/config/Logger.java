25d9992408c21ab6687753648fca47ad99dba679
Sanchit <saanchithefriend@gmail.com>
2015-03-12 20:53:15 -0400

    Yet some more junk.

---
+++ src/edu/ufl/cise/config/Logger.java
@@ -0,0 +1,41 @@
+package edu.ufl.cise.config;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Logger {
+
+	// Ensures a single instance for the logger
+	private static Logger instance = null;
+
+	private Logger() {
+	}
+
+	public static Logger getInstance() {
+		if (instance == null) {
+			instance = new Logger();
+		}
+		return instance;
+	}
+
+	public static void log(String peerId, String message) {
+		String fileName = "log_peer_" + peerId;
+		BufferedWriter bw = null;
+		try {
+			try {
+				bw = new BufferedWriter(new FileWriter(fileName));
+				bw.write(message);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} finally {
+			try {
+				if (bw != null)
+					bw.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
+}
