628cdb63978afaa79b893347a05e45f3196da159
sanchit gupta <saanchithefriend@gmail.com>
2015-04-14 00:22:09 -0400

    updated peerprocess 2 and 3

--- src/peerProcess2.java
+++ src/peerProcess2.java
@@ -9,6 +9,7 @@ import edu.ufl.cise.client.ScheduleNeighborTimerTask;
 import edu.ufl.cise.config.MetaInfo;
 import edu.ufl.cise.config.PeerInfo;
 import edu.ufl.cise.util.CommonConfigReader;
+import edu.ufl.cise.util.FileHandlingUtils;
 import edu.ufl.cise.util.PeerConfigReader;
 
 public class peerProcess2 {
@@ -25,17 +26,16 @@ public class peerProcess2 {
 
 	public static void main(String args[]) throws IOException {
 		// String peerId = args[0];
-		String peerId = "1";
+		String peerId = "2";
 		peerID = peerId;
-		base_path = System.getProperty("user.home") + "/";
+		base_path = System.getProperty("user.home") + "/project" + peerId + "/";
 		// System.out.println(base_path);
 		init(peerId);
 	}
 
 	public static void init(String peerId) throws IOException {
-		String directory = "project" + peerId + "/";
 		// Reads the Common Config file
-		CommonConfigReader.configReader(COMMON_CONFIG, base_path + directory);
+		CommonConfigReader.configReader(COMMON_CONFIG, base_path );
 
 		// Sets the number of pieces
 		int fileSize = MetaInfo.getFileSize();
@@ -48,7 +48,7 @@ public class peerProcess2 {
 		MetaInfo.setBasePath(base_path);
 
 		// Create a log file and set the path
-		String logFile = base_path + directory + "peer_" + peerID + ".log";
+		String logFile = base_path + "peer_" + peerID + ".log";
 		MetaInfo.setLogPath(logFile);
 		createLogFile();
 
@@ -57,21 +57,22 @@ public class peerProcess2 {
 
 		// Reads the peerConfig file
 		LinkedHashMap<Integer, PeerInfo> peerMap;
-		peerMap = PeerConfigReader.configReader(PEER_CONFIG, base_path
-				+ directory, Integer.parseInt(peerId));
+		peerMap = PeerConfigReader.configReader(PEER_CONFIG, base_path, Integer.parseInt(peerId));
 
-		System.out.println(MetaInfo.getString());
-		
 		// Initializes PeerClient
-/*		int peerIdInt = Integer.parseInt(peerId);
+		int peerIdInt = Integer.parseInt(peerId);
 		Peer.getInstance().init(peerIdInt, peerMap);
 
-		// Set isCompleteFlag
-		boolean isCompleteFile = Peer.getInstance().getMap().get(peerId)
-				.isCompleteFile();
-		MetaInfo.setCompletefile(isCompleteFile);
+		// Break the file into pieces
+		if(MetaInfo.isCompletefile()){
+			FileHandlingUtils fh = new FileHandlingUtils();
+			fh.createPieces();
+			fh = null;
+		}
+		
+		System.out.println(MetaInfo.getString());
 
-		// Starts PeerServer
+/*		// Starts PeerServer
 		Peer.getInstance().Serverinit();
 
 		// Starts PeerClient
